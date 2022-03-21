b08910999750007ded56a511d58a94a1bdbe8c12
haneen <sweet_hano_2008@hotmail.com>
2022-03-21 10:33:32 +0300

    settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("test"),
+    );
+  }
+}
