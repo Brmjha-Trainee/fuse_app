6596829052dcbdbad878baae438199bb52476f53
RawabiAldossary <46569022+RawabiAldossary@users.noreply.github.com>
2022-04-04 16:20:26 +0300

    Update forget_password2_vm.dart

---
+++ lib/view_model/forget_password2_vm.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:fuseapp/theme/theme_constants.dart';
+
+import 'Sign_up1.dart';
+import 'home_page.dart';
+
+class ResetPasswordPage2 extends StatefulWidget {
+   const  ResetPasswordPage2({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _ResetPasswordPageState2 createState() => _ResetPasswordPageState2();
+}
+
+class _ResetPasswordPageState2 extends State<ResetPasswordPage2> {
+
+  bool showPassword = true;
+  bool showConfirmPassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reset Password'),
+           iconTheme: IconThemeData(
+    color: Colors.black, //change your color here
+  ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          // adding spaces
+             SizedBox(
+                height: 28,
+              ),
+              Text('New password' , style: Theme.of(context).textTheme.bodyText1,),
+               SizedBox(
+                height: 10,
+              ),
+            TextField(
+              obscureText: showPassword,
+              decoration: InputDecoration(
+                hintText: 'Enter password',
+                // this item enable the user to hide and show his password
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.remove_red_eye),
+                  onPressed: () => setState(() => showPassword = !showPassword),
+                ),
+              ),
+            ),
+            SizedBox(
+                height: 18,
+              ),
+              Text('Confirm password' , style: Theme.of(context).textTheme.bodyText1,),
+               SizedBox(
+                height: 10,
+              ),
+            TextField(
+              obscureText: showConfirmPassword,
+              decoration: InputDecoration(
+                hintText: 'Enter your password',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.remove_red_eye),
+                  onPressed: () => setState(
+                      () => showConfirmPassword = !showConfirmPassword),
+                ),
+              ),
+            ),
+           SizedBox(
+                height: 28,
+              ),
+  ElevatedButton(onPressed: () { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );}, child: Text('Reset password')),
+          ],),),);}}
