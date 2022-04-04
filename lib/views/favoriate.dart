1cb0ab5503f6443f94a827a56133029c22840e12
RawabiAldossary <46569022+RawabiAldossary@users.noreply.github.com>
2022-04-05 00:26:30 +0300

    Update favoriate.dart

---
+++ lib/views/favoriate.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class favourite extends  StatelessWidget {
+  const favourite({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+} 
