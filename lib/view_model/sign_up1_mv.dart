7a8bc62e71dbd28859c9bacfc1fea83a97c7eae5
RawabiAldossary <46569022+RawabiAldossary@users.noreply.github.com>
2022-04-04 17:56:20 +0300

    Update sign_up1_mv.dart

--- lib/view_model/sign_up1_mv.dart
+++ lib/view_model/sign_up1_mv.dart
@@ -137,3 +137,42 @@ Widget build(BuildContext context) {
       ),
     );
   }
+Widget _textFieldOTP({required bool first, last}) {
+    return Container (
+      height: 85,
+       //shadowColor:LIGHT_GREY,
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: TextField(
+         
+
+          autofocus: true,
+          onChanged: (value) {
+            if (value.length == 1 && last == false) {
+              FocusScope.of(context).nextFocus();
+            }
+            if (value.length == 0 && first == false) {
+              FocusScope.of(context).previousFocus();
+            }
+          },
+          showCursor: false,
+          readOnly: false,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          keyboardType: TextInputType.number,
+          maxLength: 1,
+          decoration: InputDecoration(
+            
+            counter: Offstage(),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 1, color: Colors.white),
+                borderRadius: BorderRadius.circular(12)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 1, color: LIGHT_GREY),
+                borderRadius: BorderRadius.circular(12)),
+   
+          ),
+        ),
+      ),
+    );
+  }
