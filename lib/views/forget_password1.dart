699cd2bd949442efc5fac1e5f08f0f2d40e42ac7
RawabiAldossary <46569022+RawabiAldossary@users.noreply.github.com>
2022-04-20 23:17:16 +0300

    Update forget_password1.dart

--- lib/views/forget_password1.dart
+++ lib/views/forget_password1.dart
@@ -16,14 +16,7 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: true,
-        appBar: AppBar(
-          title: Text(
-            'Reset Password',
-          ),
-          iconTheme: IconThemeData(
-            color: Colors.black, //change your color here
-          ),
-        ),
+        appBar: myAppBar(context, title: 'Reset Password'),
         body: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
