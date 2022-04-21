80436b80fabe177c1fffda9d40558bb6a8bbeb20
RawabiAldossary <46569022+RawabiAldossary@users.noreply.github.com>
2022-04-21 22:39:50 +0300

    Update forget_password2.dart

--- lib/views/forget_password2.dart
+++ lib/views/forget_password2.dart
@@ -1,7 +1,9 @@
 //UI rawabi, BE fiazah
 import 'package:flutter/material.dart';
 import 'package:fuseapp/theme/theme_constants.dart';
-
+import 'package:provider/provider.dart';
+import '../providers/toggle_text.dart';
+import '../utils/forms_validations.dart';
 class ResetPasswordPage2 extends StatefulWidget {
   const ResetPasswordPage2({
     Key? key,
@@ -10,73 +12,75 @@ class ResetPasswordPage2 extends StatefulWidget {
   @override
   _ResetPasswordPageState2 createState() => _ResetPasswordPageState2();
 }
-
 class _ResetPasswordPageState2 extends State<ResetPasswordPage2> {
-  bool showPassword = true;
-  bool showConfirmPassword = true;
-//Fixme rawabi use the theme created by asmaa
+  final TextEditingController _passController = TextEditingController();
+    final TextEditingController _confirmPassController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+//done -Fixme rawabi use the theme created by asmaa
   //fixme fiazah store new password in DB on success redirect to login page
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: myAppBar(context, title: 'Reset Password'),
+         appBar: myAppBar(context, title: 'Reset Password'),
       body: Container(
         padding: const EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'New password',
-                style: Theme.of(context).textTheme.bodyText1,
-                textAlign: TextAlign.left,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextField(
-              obscureText: showPassword,
-              decoration: InputDecoration(
-                hintText: 'Enter password',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.remove_red_eye),
-                  onPressed: () => setState(() => showPassword = !showPassword),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 18,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Confirm password',
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextField(
-              obscureText: showConfirmPassword,
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.remove_red_eye),
-                  onPressed: () => setState(
-                      () => showConfirmPassword = !showConfirmPassword),
-                ),
-              ),
-            ),
+  Form(
+              key: _formKey,
+              child: Consumer<ToggleText>(builder: (context, toggle, _) {
+                return Column(
+                  children: <Widget>[
+                   
+                    inputText(
+                      label: 'New password',
+                      hintText: 'Enter your password',
+                      controller: _passController,
+                      keyboardType: TextInputType.visiblePassword,
+                      validation: (val) {
+                        return validatePass(_passController.text.trim());
+                      },
+                      obscureText: toggle.obscureText3,
+                      iconButton: IconButton(
+                        icon: Icon(
+                          toggle.obscureText3
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          toggle.togglePasswordStat3();
+                        },
+                      ),
+                    ),
+                    inputText(
+                      label: 'Confirm Password',
+                      hintText: 'Enter your password',
+                      obscureText: toggle.obscureText4,
+                      controller: _confirmPassController,
+                      validation: (dynamic val) {
+                        if (val.isEmpty) {
+                          return "This field is required";
+                        }
+                        return confirmPassValidation.validateMatch(
+                            val, _passController.text.trim());
+                      },
+                      iconButton: IconButton(
+                        icon: Icon(
+                          toggle.obscureText4
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          toggle.togglePasswordStat4();
+                        },
+                      ),
+                    ),
             SizedBox(
               height: 28,
             ),
             darkBtn(onPressed: () {}, label: "Reset password"),
           ],
-        ),
-      ),
-    );
-  }
+
+        );}))])));}
 }
