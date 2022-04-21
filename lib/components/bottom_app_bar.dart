61395ffec0732aeee35316bdbc4f5d0f948f9a65
RawabiAldossary <46569022+RawabiAldossary@users.noreply.github.com>
2022-04-21 22:22:05 +0300

    Update bottom_app_bar.dart

--- lib/components/bottom_app_bar.dart
+++ lib/components/bottom_app_bar.dart
@@ -22,29 +22,36 @@ class _BottomNavigationState extends State<BottomNavigation> {
   ];
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+     Widget build(BuildContext context) => Scaffold(
         body: IndexedStack(
           index: currentIndex,
           children: screens,
         ),
         bottomNavigationBar: BottomNavigationBar(
+          elevation: 10,
           currentIndex: currentIndex,
           onTap: (index) => setState(() => currentIndex = index),
           items: const [
             BottomNavigationBarItem(
-              icon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined ),
+                 activeIcon: Icon(Icons.home ),
+           
               label: "Home",
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.person),
+              icon: Icon(Icons.person_outline),
+              activeIcon: Icon(Icons.person),
               label: "Profile",
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart),
+              icon: Icon(Icons.shopping_cart_outlined),
+              activeIcon: Icon(Icons.shopping_cart),
               label: "Cart",
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
+                
+         icon: Icon(Icons.favorite_border_outlined, color: COLOR_PRIMARY,),
+            activeIcon: Icon(Icons.favorite, color: COLOR_PRIMARY,),
               label: "Favorites",
             ),
           ],
