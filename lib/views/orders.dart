f59b5d3ca19d4fb92d6b80d985f85f1ffdd0b3af
RawabiAldossary <46569022+RawabiAldossary@users.noreply.github.com>
2022-06-06 20:14:06 +0300

    Update orders.dart

--- lib/views/orders.dart
+++ lib/views/orders.dart
@@ -26,13 +26,91 @@ class _OrdersState extends State<Orders> {
   }
 
   Widget build(BuildContext context) {
+
+    Provider.of<OrderProvider>(context, listen: false).fetchOrders(context);
+     var obj = Provider.of<OrderProvider>(context, listen: true);
     return Scaffold(
       appBar: myAppBar(context, title: LocaleKeys.orders.tr()),
-      body: Center(
-        child: Text('data'),
+   
+   
+ body: ListView.builder(
+     padding: new EdgeInsets.only(left: 0.0, bottom: 8.0, right: 16.0),
+ itemCount:obj.amount!.length,
+  itemBuilder:
+  ( BuildContext   context , int index ){
+  return Card(
+           elevation: 10,
+                    shadowColor: Colors.black,   
+            child: new Column(
+              children: <Widget>[
+                new Padding(
+                    padding: new EdgeInsets.all(7.0),
+                          child: discription("Order No.",obj.orderno![index],h7),
+                    ),
+                    new Padding(
+                          padding: new EdgeInsets.all(7.0),
+                          child:
+                           Row(
+                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                             children: [ 
+                               discription("Date.",obj.date![index],h7),
+                                IconButton(
+                              onPressed: () {
+                                // Navigator.pushReplacementNamed(context,OrderDetatilsViewRoute ,arguments: 
+                                  Navigator.push( context,MaterialPageRoute(builder: (context) => Order_Detatils(obj , index)));
+                              },
+                              icon: Icon(
+                                Icons.arrow_forward_ios,
+                                color: GREY,
+                              ))
+                             ],
+                           ),
+                        ),
+                          new Padding(
+                          padding: new EdgeInsets.all(7.0),
+                        
+                child: discription("Total Amount.",obj.amount![index].toString(),(const TextStyle(color: GREEN,fontSize: 14,))),
+                        ),
+                      
+                        new Padding(   padding: new EdgeInsets.all(7.0),
+                          child:  obj.status![index] == "Delivered" ? Row(
+ mainAxisAlignment: MainAxisAlignment.spaceBetween,
+ children :[ 
+    Text('Deliverd' , style: TextStyle(color:GREEN, fontSize: 14)) ,   ElevatedButton(
+onPressed: () {
+
+      },
+         child: Text('Track')),
+
+         ]
+        ) :
+        Row(
+ children :[ 
+    Text('Canceled' , style: TextStyle(color:RED, fontSize: 14),) ,
+ ] 
+    ))
+                    
+              ],
+            ),
+          );
+}
+
       ),
-    );
+    )
+  ;
   }
+}
+ Row discription(String label , String orderdet ,TextStyle h) {
+    return Row(
+children :[
+  Text(label, style:  h5),
+  SizedBox(width: 10),
+  Text (
+  orderdet, style:  h
+  ),
+]);
+
+}
 
   Widget buildRating() => Consumer<ToggleText>(builder: (context, val, _) {
         return RatingBar.builder(
